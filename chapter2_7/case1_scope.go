c57f687d9e14a7118009478c5ec413a53b9c10ef
Zhang Fei <zhangfei@hoodinn.com>
2021-09-26 21:00:34 +0800

    chapter2_7 case

---
+++ chapter2_7/case1_scope.go
@@ -0,0 +1,97 @@
+package chapter2_7
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var g = "g"
+
+func f() {}
+
+func ScopeTest1() {
+	f := "f"
+	fmt.Println(f) // "f"; local var f shadows package-level func f
+	fmt.Println(g) // "g"; package-level var
+	// fmt.Println(h) // compile error: undefined: h
+}
+
+func ScopeTest2() {
+	x := "hello!"
+	for i := 0; i < len(x); i++ {
+		x := x[i]
+		if x != '!' {
+			x := x + 'A' - 'a'
+			fmt.Printf("%c", x) // "HELLO" (one letter per iteration)
+		}
+	}
+}
+
+func ScopeTest3() {
+	x := "hello"
+	for _, x := range x {
+		x := x + 'A' - 'a'
+		fmt.Printf("%c", x) // "HELLO" (one letter per iteration)
+	}
+}
+
+func f1() int {
+	return 0
+}
+
+func g1(x int) int {
+	return 0
+}
+
+func ScopeTest4() {
+	if x := f1(); x == 0 {
+		fmt.Println(x)
+	} else if y := g1(x); x == y {
+		fmt.Println(x, y)
+	} else {
+		fmt.Println(x, y)
+	}
+	//fmt.Println(x, y) // compile error: x and y are not visible here
+}
+
+func ScopeTest5() error {
+	var fname string
+	// f只在if语句可见
+	// if f, err := os.Open(fname); err != nil { // compile error: unused: f
+	// 	return err
+	// }
+	// f.ReadByte() // compile error: undefined f
+	// f.Close()    // compile error: undefined f
+
+	f, err := os.Open(fname)
+	if err != nil {
+		return err
+	}
+	// f.ReadByte()
+	f.Close()
+
+	return nil
+}
+
+var cwd string
+
+func init() {
+	// cwd内部申明屏蔽了外部申明
+	cwd, err := os.Getwd() // compile error: unused: cwd
+	if err != nil {
+		log.Fatalf("os.Getwd failed: %v", err)
+	}
+	// 使用cwd去掉compile error,但是全局变量并不会被正确地初始化,好在vscode会有提示,否则看似正常的日志输出会使得这个bug更加隐晦!
+	log.Printf("Working directory = %s", cwd) // 去掉这句试试
+}
+
+// 避免上述隐晦bug的方式:单独申明err,以防止:=的简短申明方式
+func init() {
+	var err error
+	cwd, err = os.Getwd()
+	if err != nil {
+		log.Fatalf("os.Getwd failed: %v", err)
+	}
+	// 这里不打印cwd(使用cwd),是为了凸显上面的错误!
+}
