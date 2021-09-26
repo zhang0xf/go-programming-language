c1f809f489554e92b136cf9ff6ffb75bcf9a749b
Zhang Fei <zhangfei@hoodinn.com>
2021-09-26 17:16:22 +0800

    update case

--- chapter2_5/case1_tempconv.go
+++ chapter2_5/case1_tempconv.go
@@ -2,7 +2,11 @@
 
 // Celsius 与 Fahrenheit 是不同的数据类型,不可以相互比较和混在一个表达式中,需要提供显示转换CToF()和FToC()
 
+// 类型转换改变了语义(编译器:语义规则,语法分析)
+
 package tempconv
+
+import "fmt"
 
 type Celsius float64    // 摄氏温度
 type Fahrenheit float64 // 华氏温度
@@ -16,3 +20,20 @@ const (
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+func TypeTest() {
+
+	// 内置运算
+	fmt.Printf("%g\n", BoilingC-FreezingC) // "100" °C
+	boilingF := CToF(BoilingC)
+	fmt.Printf("%g\n", boilingF-CToF(FreezingC)) // "180" °F
+	// fmt.Printf("%g\n", boilingF-FreezingC)       // compile error: type mismatch
+
+	// == 与 <
+	var c Celsius
+	var f Fahrenheit
+	fmt.Println(c == 0)          // "true"
+	fmt.Println(f >= 0)          // "true"
+	fmt.Println(c == Celsius(f)) // "true"!
+	// fmt.Println(c == f)          // compile error: type mismatch
+}
