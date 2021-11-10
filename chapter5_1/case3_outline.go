47b257be8310352c1cce018d9a3dddc953aef79e
Zhang Fei <zhangfei@hoodinn.com>
2021-11-10 16:18:49 +0800

    chapter5_1 case

--- chapter5_1/case3_outline.go
+++ chapter5_1/case3_outline.go
@@ -7,6 +7,8 @@ import (
 	"golang.org/x/net/html"
 )
 
+// 当outline调用自身时，被调用者接收的是stack的拷贝。
+// 被调用者对stack的元素追加操作，修改的是stack的拷贝,但这个过程并不会修改调用方的stack。(即:父节点和每一个子节点拼接)
 func outline(stack []string, n *html.Node) {
 	if n.Type == html.ElementNode {
 		stack = append(stack, n.Data) // push tag
