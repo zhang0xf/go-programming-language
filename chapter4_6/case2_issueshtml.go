1d88e347f7f94e5e0a3bfe4577638736cf9cf326
Zhang Fei <zhangfei@hoodinn.com>
2021-11-10 11:56:53 +0800

    chapter4_6 case

--- chapter4_6/case2_issueshtml.go
+++ chapter4_6/case2_issueshtml.go
@@ -8,6 +8,10 @@ import (
 )
 
 // usage : ./exercise repo:golang/go commenter:gopherbot json encoder > data/issues.html
+// usage : ./exercise repo:golang/go 3133 10535 > data/issues2.html
+
+// html/template包已经自动将特殊字符转义，因此我们依然可以看到正确的字面值。
+// 如果我们使用text/template包的话，这2个issue将会产生错误，其中“&lt;”四个字符将会被当作小于字符“<”处理，同时“<link>”字符串将会被当作一个链接元素处理.
 
 func HtmlTemplate() {
 	var issueList = template.Must(template.New("issuelist").Parse(`	
