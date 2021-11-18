85b006cd4f698270edbcaaf9eacb8154468a91ff
Zhang Fei <zhangfei@hoodinn.com>
2021-11-18 11:06:44 +0800

    chapter5_6 case

---
+++ chapter5_6/case6_rmdir.go
@@ -0,0 +1,16 @@
+package chapter5_6
+
+func RemoveDir() {
+	// var rmdirs []func()
+	// for _, d := range tempDirs() {
+	// 	dir := d               // NOTE: necessary!
+	// 	os.MkdirAll(dir, 0755) // creates parent directories too
+	// 	rmdirs = append(rmdirs, func() {
+	// 		os.RemoveAll(dir)
+	// 	})
+	// }
+	// // ...do some work…
+	// for _, rmdir := range rmdirs {
+	// 	rmdir() // clean up
+	// }
+}
