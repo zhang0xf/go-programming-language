7af86520de791c029a226e4823474c54d5acc740
Zhang Fei <zhangfei@hoodinn.com>
2021-11-24 19:30:07 +0800

    chapter7_1 case

---
+++ chapter7_1/case2_bytecounter.go
@@ -0,0 +1,22 @@
+package chapter7_1
+
+import "fmt"
+
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p)) // convert int to ByteCounter
+	return len(p), nil
+}
+
+// 因为*ByteCounter满足io.Writer的约定，我们可以把它传入Fprintf函数中；Fprintf函数执行字符串格式化的过程不会去关注ByteCounter正确的累加结果的长度。
+
+func ByteCount() {
+	var c ByteCounter
+	c.Write([]byte("hello"))
+	fmt.Println(c) // "5", = len("hello")
+	c = 0          // reset the counter
+	var name = "Dolly"
+	fmt.Fprintf(&c, "hello, %s", name)
+	fmt.Println(c) // "12", = len("hello, Dolly")
+}
