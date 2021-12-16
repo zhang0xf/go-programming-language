1455d643b358b35ef6226d3b9ea54e9d0b705638
Zhang Fei <zhangfei@hoodinn.com>
2021-12-16 16:56:43 +0800

    chapter9_7 case

---
+++ chapter9_7/case1_concurrent_noblocking_cache.go
@@ -0,0 +1,22 @@
+package chapter9_7
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+// 并发的非阻塞缓存
+// 本节中我们会做一个无阻塞的缓存，这种工具可以帮助我们来解决现实世界中并发程序出现但没有现成的库可以解决的问题。
+// 这个问题叫作缓存(memoizing)函数，也就是说，我们需要缓存函数的返回结果，这样在对函数进行调用的时候，我们就只需要一次计算，之后只要返回计算的结果就可以了。
+// 我们的解决方案会是并发安全且会避免对整个缓存加锁而导致所有操作都去争一个锁的设计。
+
+// 我们将使用下面的httpGetBody函数作为我们需要缓存的函数的一个样例。
+// 这个函数会去进行HTTP GET请求并且获取http响应body。对这个函数的调用本身开销是比较大的，所以我们尽量避免在不必要的时候反复调用。
+func httpGetBody(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
---
+++ chapter9_7/case2_memo1.go
@@ -0,0 +1,36 @@
+// Package memo provides a concurrency-unsafe
+// memoization of a function of type Func.
+package chapter9_7
+
+// 下面是我们要设计的cache的第一个“草稿”：
+// Memo实例会记录需要缓存的函数f(类型为Func)，以及缓存内容(里面是一个string到result映射的map)。
+// 每一个result都是简单的函数返回的值对儿--一个值和一个错误值。
+// 继续下去我们会展示一些Memo的变种，不过所有的例子都会遵循上面的这些方面。
+
+// A Memo caches the results of calling a Func.
+type Memo struct {
+	f     Func
+	cache map[string]result
+}
+
+// Func is the type of the function to memoize.
+type Func func(key string) (interface{}, error)
+
+type result struct {
+	value interface{}
+	err   error
+}
+
+func New(f Func) *Memo {
+	return &Memo{f: f, cache: make(map[string]result)}
+}
+
+// NOTE: not concurrency-safe!
+func (memo *Memo) Get(key string) (interface{}, error) {
+	res, ok := memo.cache[key]
+	if !ok {
+		res.value, res.err = memo.f(key)
+		memo.cache[key] = res
+	}
+	return res.value, res.err
+}
